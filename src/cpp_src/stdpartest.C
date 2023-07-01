9c7b058582a03c7b997b309f5a341b00bf58a74b
Miguel R Zuniga <Miguel.Zuniga@noaa.gov>
2023-06-30 23:47:50 -0400

    Added search_copy_if function working serial and for gpu using std::copy_if.

--- src/cpp_src/stdpartest.C
+++ src/cpp_src/stdpartest.C
@@ -5,81 +5,182 @@
 #include <limits>
 #include <string>
 #include <vector>
-//#include <cartesian_product.hpp> // Brings C++23 std::views::cartesian_product to C++20
-#include <numeric>   // For std::transform_reduce
 #include <ranges>
 #include <execution>
 #include <random>
+#include <array>
+#include <numeric>
+//#include <cartesian_product.hpp> 
+//#include <numeric>   // used with std::transform_reduce
+//#include <future>
 
+//TODO: make and test the homegrown copy_if that uses atomics as
+// alternative to std::copy_if
 
 using namespace std;
+using namespace std::chrono;
+
+
+constexpr size_t NX{1000000};
+constexpr int MAX_NN{100};
+constexpr int MAX_NNP{MAX_NN+1};
+constexpr int MAX_INT{std::numeric_limits<int>::max()};
+constexpr float max_distance {2.25};
 
 bool is_near(float x, float y){
-    if (sqrt((y-x)*(y-x)) <= 0.25){
+    if (sqrt((y-x)*(y-x)) <= max_distance){
         return true;
     }else{
         return false;
     }
 }
 
-void initialize(vector<float> &x, vector<float> &y, vector<vector<size_t>>& y_near_x, const size_t nx, const size_t maxy) {
+
+
+void initialize_data(vector<float> &x, vector<float> &y) {
     std::random_device  rd;
     std::mt19937        gen(rd());
     std::uniform_real_distribution dis(-0.25, 0.25);
-
-    y_near_x.reserve(nx);
-    for (int i = 0; i < nx; i++) {
-      vector<size_t> vt;
-      vt.reserve(maxy);
-
-      for(int j = 0; j<maxy; j++){
-        vt[j] = nx+1;
-      }
-      y_near_x.push_back(vt);
-    }
-
-    y.reserve(nx);
-    x.reserve(nx);
-
-    auto ints = std::views::iota(0);//TODO: cast?
-    std::for_each_n(std::execution::par, ints.begin(), x.size(), 
-    [x = x.data(), y=y.data()](int i) {
+     
+    auto ints = std::views::iota((size_t)0, x.size());
+    cout <<"calling for each n"<< endl;
+    std::for_each_n(std::execution::seq, ints.begin(), x.size(),  [x = x.data(), y=y.data()](int i) {
       x[i] = (double)i;
       if ((i % 2) == 0) {
-        y[i] = 0;
+        y[i] = MAX_INT; // some large number
       } else {
         y[i] = (double)i;// + dis(gen);
       }
     });
-    // std::fill_n
   }
 
 
-void search(vector<float> &x, vector<float> &y, vector<vector<size_t>>& y_near_x) {
-  auto ints = std::views::iota(0, (int)x.size());
-  
-  std::for_each_n(std::execution::par, 
-    ints.begin(), x.size(), [&](int i) {
-        for(int j{0}; j< y.size(); ++j){
-         if(is_near(x[i], y[j])){ 
-            //y_near_x[i].push_back(j);
-            //y_near_x[i].emplace_back(j);
-            assert(y_near_x[i].size() < y_near_x[i].capacity());
-            y_near_x[i][y_near_x[i].size()] = j;
+void initialize_results_arrays(vector<float> &x, vector<array<int,MAX_NNP>>& y_n_x  ) {
+    y_n_x.reserve(x.size());
+     for (int i = 0; i < x.size() ; i++){
+        for (int j = 0; j < MAX_NN; j++){
+          y_n_x[i][j] = MAX_NN;
+        }
+        y_n_x[i][MAX_NN] = 0;
+    }
+  }
 
+void initialize_results_vec(vector<float> &x,  vector<vector<int>>& y_near_x  ) {
+    y_near_x.reserve(x.size());
+    for (auto i = 0; i < x.size() ; i++){
+        y_near_x[i].reserve(MAX_NN);
+        for (auto j = 0; j < MAX_NN; j++){
+          y_near_x[i][j] = MAX_NNP;
+        }
+    }
+}
+
+void print_results (vector<vector<int>>& y_near_x){
+for (auto i = 0;  i < y_near_x.size(); i++){
+     for(auto j = 0; j<y_near_x[i].size(); j++)
+       cout << "i , j, y_near_x : " << i <<", " << j <<", " << y_near_x[i][j] <<endl;
+    }
+}
+
+void compare_results(vector<vector<int>> &r1, vector<vector<int>> &r2) {
+    auto nof_errors{0};
+    if (r1.size() != r2.size()) {
+     cout << "Error r1.size() != r2.size()" << endl;
+     return;
+    }
+
+    for (auto i{0}; i < r1.size(); i++) {
+     std::sort(r1[i].begin(), r1[i].end());
+     std::sort(r2[i].begin(), r2[i].end());
+     if (r1[i] != r2[i]){
+       cout << "Error r1[i] != r2[i] ffor i=" << i << endl;
+        nof_errors++;
+    }
+    }
+
+    cout << "compare_results found " << nof_errors << " discrepancies." << endl;
+}
+
+void search_serial(vector<float> &x, vector<float> &y, vector<vector<int>> &y_near_x) {
+    auto ints = std::views::iota(0, (int)x.size());
+    auto NY = y.size();
+    std::for_each_n(
+        std::execution::seq,
+        ints.begin(), x.size(),
+        [&, x = x.data(), y = y.data()](int i) {
+          for (auto j{0}; j < NY; ++j) {
+            if (is_near(x[i], y[j])) {
+              y_near_x[i].push_back(j);
+            }
+          }
+        });
+  }
+
+void search_copy_if(vector<float> &x, vector<float> &y, vector<vector<int>> &y_near_x) {
+  auto ints = std::views::iota(0, (int)y.size());  // NOTE: its y.size(); and assuming reuse in loop for i (x)
+  for (int i = 0; i < x.size(); i++) {
+    auto nn_iter =  y_near_x[i].begin();
+     std::copy_if(
+         std::execution::par,
+         // NOTE: nvc++ w/ GPU requires random_access iterators for 3rd (result) arg.
+         ints.begin(), ints.end(), nn_iter,
+         [=, x = x.data(), y = y.data()](int j) {
+           if (is_near(x[i], y[j])) {
+             return true;
+           } else {
+             return false;
+           }
+         });
+  }
+}
+
+void search_for_each(vector<float> &x, vector<float> &y, vector<array<int,MAX_NNP>>& y_near_x) {
+  assert(1 == 0); //TODO: unfinished - do not use. Alternative to copy_if?
+  auto ints = std::views::iota(0, (int)x.size());
+  auto NY = y.size();
+  std::for_each_n(std::execution::seq, 
+    ints.begin(), x.size(), [&, x = x.data(), y = y.data()](int i) {
+        for(int j{0}; j< NY ; ++j){
+         if(is_near(x[i], y[j])){ 
+            //TODO: lock needed
+            assert(y_near_x[i][MAX_NN] < MAX_NN);
+            y_near_x[i][ y_near_x[i][MAX_NN] ] = j;
+            ++y_near_x[i][MAX_NN];
+        
           }
         }
      });
-  }
-
-
+}
 int main(int argc, char *argv[]) {
-  const size_t NX{100};
-  const size_t MY{10};
-    vector<float> x,y;
-    vector<vector<size_t>> y_near_x;
-    vector<array<int,10>> y_n_x;
-    initialize(x,y,y_near_x,NX,MY);
-    search(x,y,y_near_x);
+  vector<float> x(NX, 0.), y(NX, 0.);  // allocation of same size and init
+  vector<vector<int>> y_near_x_r1, y_near_x_r2;
+  vector<array<int, MAX_NNP>> y_n_x;
+  initialize_data(x, y);
+  initialize_results_vec(x, y_near_x_r1);
+  initialize_results_vec(x, y_near_x_r2);
+  initialize_results_arrays(x, y_n_x);
+
+  cout << endl << "Calling search_serial" << endl;
+  auto start =  high_resolution_clock::now();
+  search_serial(x, y, y_near_x_r1);
+  auto stop =  high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+  cout << "serial search time: "
+       << duration.count() / 1.0e6 << " seconds" << endl;
+  //print_results(y_near_x_r1);
+
+  cout << "Calling search_copy_if" << endl;
+  start =  high_resolution_clock::now();
+  search_copy_if(x, y, y_near_x_r2);
+  stop =  high_resolution_clock::now();
+  duration = duration_cast<microseconds>(stop - start);
+  cout << "paralle search time: "
+    << duration.count() / 1.0e6 << " seconds" << endl;
+  //print_results(y_near_x_r2);
+
+  compare_results(y_near_x_r1, y_near_x_r2);
 
 }
+
+
+
